b0c45e36df86b5b4d813b4f549455e4420e94965
DavidChen617 <a880617c@gmail.com>
2026-03-20 22:01:19 +0800

    feat(39): add combination sum solution

---
+++ Chsarp/src/NeedCode/39_CombinationSum/Solution1.cs
@@ -0,0 +1,30 @@
+﻿namespace NeedCode._39_CombinationSum;
+
+public class Solution1
+{
+    public List<List<int>> CombinationSum(int[] nums, int target)
+    {
+        var result = new List<List<int>>();
+        var sub = new List<int>();
+        DFS(0, 0);
+
+        return result;
+        
+        void DFS(int index, int total)
+        {
+            if (total == target)
+            {
+                result.Add(new List<int>(sub));
+                return;
+            }
+            
+            if(total > target || index >= nums.Length)
+                return;
+            
+            sub.Add(nums[index]);
+            DFS(index, total + nums[index]); // 選
+            sub.RemoveAt(sub.Count - 1); // 反悔, 拿掉最後一個
+            DFS(index + 1, total); // 下一步
+        }
+    }
+}
