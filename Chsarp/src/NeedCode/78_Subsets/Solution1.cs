cc33e27c928ec65225657d414853b999e6e05c67
DavidChen617 <a880617c@gmail.com>
2026-03-20 21:08:05 +0800

    feat(78): add subsets solution

---
+++ Chsarp/src/NeedCode/78_Subsets/Solution1.cs
@@ -0,0 +1,27 @@
+﻿namespace NeedCode._78_Subsets;
+
+public class Solution1
+{
+    public List<List<int>> Subsets(int[] nums)
+    {
+        var result = new List<List<int>>();
+        var sub = new List<int>();
+
+        DFS(0);
+        return result;
+
+        void DFS(int i)
+        {
+            if (i >= nums.Length)
+            {
+                result.Add(new (sub));
+                return;
+            }
+
+            sub.Add(nums[i]); // 選
+            DFS(i + 1); // 往下走
+            sub.RemoveAt(sub.Count - 1); // 不選
+            DFS(i + 1); // 往下走
+        }
+    }
+}
