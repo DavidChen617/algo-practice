4154c6aa146f50e16b4215af1edaaeeae267e73c
DavidChen617 <a880617c@gmail.com>
2026-03-21 14:27:50 +0800

    feat(22): add GenerateParenthesis solution1

---
+++ Chsarp/src/NeedCode/22_GenerateParenthesis/Solution1.cs
@@ -0,0 +1,25 @@
+﻿namespace NeedCode._22_GenerateParenthesis;
+
+public class Solution1
+{
+    public List<string> GenerateParenthesis(int n)
+    {
+        var result = new List<string>();
+        DFS(0, 0, string.Empty);
+        return result;
+
+        void DFS(int left, int right, string stack)
+        {
+            if (left == n && left == right)
+            {
+                result.Add(stack);
+                return;
+            }
+
+            if (left < n)
+                DFS(left + 1, right, stack + '(');
+            if(right < left)
+                DFS(left, right + 1, stack + ')');
+        }
+    }
+}
