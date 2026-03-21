19bef40bcc3c1750dcc24315e808025370529106
DavidChen617 <a880617c@gmail.com>
2026-03-21 13:24:02 +0800

    feat(46): add permute solution1

---
+++ Chsarp/src/NeedCode/46_Permute/Solution1.cs
@@ -0,0 +1,25 @@
+﻿namespace NeedCode._46_Permute;
+
+public class Solution1
+{
+    public List<List<int>> Permute(int[] nums)
+    {
+        if (nums.Length == 0)
+            return new() { new() };
+        var per = Permute(nums[1..]);
+        var result = new List<List<int>>();
+        var insertValue = nums[0];
+        
+        for (int i = 0; i < per.Count; i++)
+        {
+            for (int j = 0; j <= per[i].Count; j++)
+            {
+                var copy = new List<int>(per[i]);
+                copy.Insert(j, insertValue);
+                result.Add(copy);
+            }
+        }
+
+        return result;
+    }
+}
