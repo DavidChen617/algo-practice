004ae69cde0d4a26a0911b4568e117dcfe8130e6
DavidChen617 <a880617c@gmail.com>
2026-03-21 12:40:01 +0800

    feat(40): add combination sum2 solution

---
+++ Chsarp/src/NeedCode/40_CombinationSum2/Solution1.cs
@@ -0,0 +1,35 @@
+﻿namespace NeedCode._40_CombinationSum2;
+
+public class Solution1
+{
+    public List<List<int>> CombinationSum2(int[] candidates, int target)
+    {
+        Array.Sort(candidates);
+        var res = new List<List<int>>();
+        var sub = new List<int>();
+        DFS(0, 0);
+
+        return res;
+
+        void DFS(int index, int total)
+        {
+            if (total == target)
+            {
+                res.Add(new (sub));
+                return;
+            }
+            
+            if(index >= candidates.Length || total > target)
+                return;
+            
+            sub.Add(candidates[index]); // 選
+            DFS(index + 1, total + candidates[index]);
+            sub.RemoveAt(sub.Count - 1);
+            var next = index + 1;
+            while(next < candidates.Length && candidates[next] == candidates[index])
+                ++next;
+            
+            DFS(next, total);
+        }
+    }
+}
