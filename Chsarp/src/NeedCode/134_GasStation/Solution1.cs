9e60194ce878006da6d0fca721020cf9b5fe348b
DavidChen617 <a880617c@gmail.com>
2026-03-23 12:13:17 +0800

    feat(134): add gas station solutions

---
+++ Chsarp/src/NeedCode/134_GasStation/Solution1.cs
@@ -0,0 +1,33 @@
+﻿namespace NeedCode._134_GasStation;
+
+public class Solution1
+{
+    public int CanCompleteCircuit(int[] gas, int[] cost)
+    {
+        int n = gas.Length;
+        
+        for (int start = 0; start < n; start++)
+        {
+            var tank = gas[start] - cost[start];
+            if (tank < 0)
+                continue;
+            
+            int cycle = (start + 1) % n;
+            while (cycle != start)
+            {
+                tank += gas[cycle] - cost[cycle];
+                if(tank < 0)
+                    break;
+                
+                cycle = (cycle + 1) % n;
+            }
+
+            if (cycle == start)
+            {
+                return start;
+            }
+        }
+
+        return -1;
+    }
+}
