7c38f2dfe07925fd9774f16e668ecbe579dfd7d3
DavidChen617 <a880617c@gmail.com>
2026-03-23 10:27:25 +0800

    feat(45): add canJump2 solutions

---
+++ Chsarp/src/NeedCode/45_CanJump2/Solution1.cs
@@ -0,0 +1,29 @@
+﻿namespace NeedCode._45_CanJump2;
+
+public class Solution1
+{
+    public int Jump(int[] nums)
+    {
+        var n = nums.Length - 1;
+        return DFS(0);
+
+        int DFS(int index)
+        {
+            if (index == n)
+                return 0;
+
+            if (nums[index] == 0)
+                return int.MaxValue;
+            
+            var end = Math.Min(n, index + nums[index]);
+            var res = int.MaxValue;
+
+            for (int i = index + 1; i <= end; i++)
+            {
+                res = Math.Min(res, 1 + DFS(i));
+            }
+            
+            return res;
+        }
+    }
+}
