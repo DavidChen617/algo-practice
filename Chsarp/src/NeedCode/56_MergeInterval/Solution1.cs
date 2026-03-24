d5f2305a7aa4bda142016394e58de8bf563555a9
DavidChen617 <a880617c@gmail.com>
2026-03-24 17:45:13 +0800

    feat(56): add MergeInterval solution1

---
+++ Chsarp/src/NeedCode/56_MergeInterval/Solution1.cs
@@ -0,0 +1,40 @@
+﻿namespace NeedCode._56_MergeInterval;
+
+public class Solution1
+{
+    public int[][] Merge(int[][] intervals)
+    {
+        Array.Sort(intervals, (a, b) => a[0].CompareTo(b[0]));
+        var result = new List<int[]>();
+        result.Add(intervals[0]);
+
+        for (int i = 0; i < intervals.Length; i++)
+        {
+            var interval = intervals[i];
+            int start = interval[0],
+                end = interval[1],
+                lastEnd = result[result.Count - 1][1];
+
+            if (start <= lastEnd)
+            {
+                result[result.Count - 1][1] = Math.Max(end, lastEnd);
+            }
+            else
+            {
+                result.Add(interval);
+            }
+        }
+
+        return result.ToArray();
+    }
+}
+
+
+/**
+ * [
+ *  [1,3],
+ *  [1,5],
+ *  [6,7]
+ * ]
+ * 
+ */
