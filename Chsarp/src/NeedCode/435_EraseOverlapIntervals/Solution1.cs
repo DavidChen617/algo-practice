5903793c3347d4445265aafe31ca4cdbbc9eb30c
DavidChen617 <a880617c@gmail.com>
2026-03-24 18:11:30 +0800

    feat(435): add EraseOverlapIntervals solution1

---
+++ Chsarp/src/NeedCode/435_EraseOverlapIntervals/Solution1.cs
@@ -0,0 +1,20 @@
+﻿namespace NeedCode._435_EraseOverlapIntervals;
+
+public class Solution1
+{
+    public int EraseOverlapIntervals(int[][] intervals) {
+        Array.Sort(intervals, (a, b) => a[1].CompareTo(b[1]));
+        int lastEnd = intervals[0][1], count = 0;
+
+        for (int i = 1; i < intervals.Length; i++)
+        {
+            int start = intervals[i][0], end = intervals[i][1];
+            if (start < lastEnd)
+                count++;
+            else
+                lastEnd = end;
+        }
+
+        return count;
+    }
+}
