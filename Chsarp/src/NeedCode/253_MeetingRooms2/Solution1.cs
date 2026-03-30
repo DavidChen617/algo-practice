b2b29d8aa11a400218cd6f1a255f13fe543a3cec
DavidChen617 <a880617c@gmail.com>
2026-03-30 20:37:36 +0800

    feat(253): add meeting rooms2 solution1

---
+++ Chsarp/src/NeedCode/253_MeetingRooms2/Solution1.cs
@@ -0,0 +1,30 @@
+﻿namespace NeedCode._253_MeetingRooms2;
+
+public class Solution1
+{
+    public int MinMeetingRooms(List<Interval> intervals)
+    {
+        intervals.Sort((a, b) => a.start.CompareTo(b.start));
+        var minQueue = new PriorityQueue<int, int>();
+
+        for (int i = 0; i < intervals.Count; i++)
+        {
+            if (minQueue.Count > 0 && minQueue.Peek() <= intervals[i].start)
+                minQueue.Dequeue();
+            minQueue.Enqueue(intervals[i].end, intervals[i].end);
+        }
+        
+        return minQueue.Count;
+    }
+}
+
+public class Interval
+{
+    public int start, end;
+
+    public Interval(int start, int end)
+    {
+        this.start = start;
+        this.end = end;
+    }
+}
