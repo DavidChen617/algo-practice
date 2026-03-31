65d4e70f1514e383c9132109120c2183b3d94628
DavidChen617 <a880617c@gmail.com>
2026-03-31 22:20:18 +0800

    feat(48): add rotateimage solution1

---
+++ Chsarp/src/NeedCode/48_RotateImage/Solution1.cs
@@ -0,0 +1,25 @@
+﻿namespace NeedCode._48_RotateImage;
+
+public class Solution1
+{
+    public void Rotate(int[][] matrix) {
+        int n = matrix.Length;
+        var rotated = new int[n][];
+        for (int i = 0; i < n; i++)
+        {
+            rotated[i] = new int[n];
+            for (int j = 0; j < n; j++)
+            {
+                rotated[i][j] = matrix[n - 1 - j][i];
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matrix[i][j] = rotated[i][j];
+            }
+        }
+    }
+}
