3176a49b187a2c74cdfb5ea2f2d33bc5e0a8319c
DavidChen617 <a880617c@gmail.com>
2026-04-02 18:07:17 +0800

    feat(73): add set matrix zeros solution1

---
+++ Chsarp/src/NeedCode/73_SetMatrixZeroes/Solution1.cs
@@ -0,0 +1,23 @@
+﻿namespace NeedCode._73_SetMatrixZeroes;
+
+public class Solution1
+{
+    public void SetZeroes(int[][] matrix)
+    {
+        (int m, int n) = (matrix.Length, matrix[0].Length);
+        bool[] rowZeros = new bool[m], colZeros = new bool[n];
+
+        for (int r = 0; r < m; ++r)
+            for (int c = 0; c < n; ++c)
+                if (matrix[r][c] == 0)
+                {
+                    rowZeros[r] = true;
+                    colZeros[c] = true;
+                }
+
+        for (int r = 0; r < m; ++r)
+            for (int c = 0; c < n; ++c)
+                if(rowZeros[r] ||  colZeros[c])
+                    matrix[r][c] = 0;
+    }
+}
