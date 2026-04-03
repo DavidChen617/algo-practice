d1d5dfe29fd4a816b71fb71603307435fabd4292
DavidChen617 <a880617c@gmail.com>
2026-04-03 10:44:04 +0800

    feat(202): add happynumber solution1

---
+++ Chsarp/src/NeedCode/202_HappyNumber/Solution1.cs
@@ -0,0 +1,25 @@
+﻿namespace NeedCode._202_HappyNumber;
+
+public class Solution1
+{
+    public bool IsHappy(int n)
+    {
+        var seed = new HashSet<int>();
+        while (n != 1)
+        {
+            if(!seed.Add(n))
+                return false;
+            
+            var output = 0;
+            while (n > 0)
+            {
+                var digit= n % 10;
+                output += digit * digit;
+                n /= 10;
+            }
+            n = output;
+        }
+        
+        return true;
+    }
+}
