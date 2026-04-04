6ebb397fe92e94c4122c22bcbe7460a103b4ac79
DavidChen617 <a880617c@gmail.com>
2026-04-04 16:51:58 +0800

    feat(338): add countingbits solution1

---
+++ Chsarp/src/NeedCode/338_CountingBits/Solution1.cs
@@ -0,0 +1,20 @@
+﻿namespace NeedCode._338_CountingBits;
+
+public class Solution1
+{
+    public int[] CountBits(int n)
+    {
+        var res = new int[n + 1];
+
+        for (int num = 0; num <= n; num++)
+        {
+            for (int i = 0; i < 32; i++)
+            {
+                if ((num & (1 << i)) != 0)
+                    ++res[num];
+            }
+        }
+        
+        return res;
+    }
+}
