559866fbf668b87e1d7417fb99dc582e0490a098
DavidChen617 <a880617c@gmail.com>
2026-04-04 15:14:03 +0800

    feat(136): add singlenumber solutions

---
+++ Chsarp/src/NeedCode/136_SingleNumber/Solution1.cs
@@ -0,0 +1,23 @@
+﻿namespace NeedCode._136_SingleNumber;
+
+public class Solution1
+{
+    public int SingleNumber(int[] nums)
+    {
+        Array.Sort(nums);
+        int i = 0;
+
+        while (i < nums.Length)
+        {
+            if (i == nums.Length - 1)
+                return nums[i];
+
+            if (nums[i] != nums[i + 1])
+                return nums[i];
+
+            i += 2;
+        }
+
+        return nums[nums.Length - 1];
+    }
+}
