6152036d2f7e4b9a308bcac9d910055668ff4b87
DavidChen617 <a880617c@gmail.com>
2026-04-04 11:11:10 +0800

    feat(50): add pow solution1

---
+++ Chsarp/src/NeedCode/50_Pow/Solution1.cs
@@ -0,0 +1,23 @@
+﻿namespace NeedCode._50_Pow;
+
+public class Solution1
+{
+    public double MyPow(double x, int n)
+    {
+        if (x == 0)
+            return 0;
+        if(n == 0)
+            return 1;
+
+        double res = 1;
+        for (int i = 0; i < Math.Abs(n); i++)
+        {
+            res *= x;
+        }
+
+        if (n < 0)
+            res = 1 / res;
+        
+        return res;
+    }
+}
