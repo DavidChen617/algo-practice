f2b0dee19a4b939a876e5933b51cc52413d7a683
DavidChen617 <a880617c@gmail.com>
2026-04-10 16:01:28 +0800

    feat(75): add missing number solution1

---
+++ Chsarp/src/NeedCode/75_MissingNumber/Solution1.cs
@@ -0,0 +1,31 @@
+﻿namespace NeedCode._75_MissingNumber;
+
+public class Solution1
+{
+    // nums = [3, 0, 1]
+    // xor ^ i ^ nums[i]
+    // xor      11
+    // i        00 
+    // nums[i]  11 
+    // = 00
+    // xor      00
+    // i        01
+    // nums[i]  00
+    // = 01
+    // xor      01     
+    // i        10
+    // nums[i]  01
+    // = 10
+    public int MissingNumber(int[] nums)
+    {
+        int n = nums.Length,
+            xor = n;
+
+        for (int i = 0; i < n; i++)
+        {
+            xor = xor ^ i ^ nums[i];
+        }
+
+        return xor;
+    }
+}
