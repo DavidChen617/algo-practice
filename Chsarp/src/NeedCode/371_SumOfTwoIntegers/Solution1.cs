1d5dd355c2b22090f98503fffc1ae47232091c27
DavidChen617 <a880617c@gmail.com>
2026-04-10 21:58:43 +0800

    feat(371): add sum of two integers solution1

---
+++ Chsarp/src/NeedCode/371_SumOfTwoIntegers/Solution1.cs
@@ -0,0 +1,16 @@
+﻿namespace NeedCode._371_SumOfTwoIntegers;
+
+public class Solution1
+{
+    public int GetSum(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = (a & b) << 1;
+            a ^= b;
+            b = temp;
+        }
+        
+        return a;
+    }
+}
